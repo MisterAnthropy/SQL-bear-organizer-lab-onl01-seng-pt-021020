b656055fb410472cff16e3b1349ff0c4ebbe6706
Tyler Croley <tjcroley@outlook.com>
2020-04-13 22:11:04 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-insert into bears (name, age, gender, color, temperament, alive) values ('Mr. Chocolate', 3,)
-insert into bears (name, age, gender, color, temperament, alive) values ('Rowdy', 1,)
-insert into bears (name, age, gender, color, temperament, alive) values ('Tabitha', 4,)
-insert into bears (name, age, gender, color, temperament, alive) values ('Sergeant Brown', 6,)
-insert into bears (name, age, gender, color, temperament, alive) values ('Melissa', 3,)
-insert into bears (name, age, gender, color, temperament, alive) values ('Grinch', 2,)
-insert into bears (name, age, gender, color, temperament, alive) values ('Wendy', 7,)
-insert into bears (name, age, gender, color, temperament, alive) values (Null, 10,)
+insert into bears (name, age, gender, color, temperament, alive) values ('Mr. Chocolate', 3,);
+insert into bears (name, age, gender, color, temperament, alive) values ('Rowdy', 1,);
+insert into bears (name, age, gender, color, temperament, alive) values ('Tabitha', 4,);
+insert into bears (name, age, gender, color, temperament, alive) values ('Sergeant Brown', 6,);
+insert into bears (name, age, gender, color, temperament, alive) values ('Melissa', 3,);
+insert into bears (name, age, gender, color, temperament, alive) values ('Grinch', 2,);
+insert into bears (name, age, gender, color, temperament, alive) values ('Wendy', 7,);
+insert into bears (name, age, gender, color, temperament, alive) values (Null, 10,);
