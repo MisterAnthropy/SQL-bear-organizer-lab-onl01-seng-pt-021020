e0d65cb3f4455f134950c7ba54906f4f3410cdf1
Tyler Croley <tjcroley@outlook.com>
2020-04-13 22:33:15 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-insert into bears (id, name, age, gender, color, temperament, alive) values ('Mr. Chocolate', 20, "M", "dark brown", "calm", 0);
-insert into bears (id, name, age, gender, color, temperament, alive) values ('Rowdy', 10, "M", "black", "intense", 1);
-insert into bears (id, name, age, gender, color, temperament, alive) values ('Tabitha', 6, "F", "dark brown", "Nice", 1);
-insert into bears (id, name, age, gender, color, temperament, alive) values ('Sergeant Brown', 19, "M", "Green", "Slimy", 0);
-insert into bears (id, name, age, gender, color, temperament, alive) values ('Melissa', 13, "F", "dark brown", "goofy", 1);
-insert into bears (id, name, age, gender, color, temperament, alive) values ('Grinch', 2, "M", "Black", "Grinchy", 1);
-insert into bears (id, name, age, gender, color, temperament, alive) values ('Wendy', 6, "F", "Blue", "naive", 1);
-insert into bears (id, name, age, gender, color, temperament, alive) values (null, 20, "M", "black", "aggressive", 0);
+insert into bears (name, age, gender, color, temperament, alive) values ('Mr. Chocolate', 20, "M", "dark brown", "calm", 0);
+insert into bears (name, age, gender, color, temperament, alive) values ('Rowdy', 10, "M", "black", "intense", 1);
+insert into bears (name, age, gender, color, temperament, alive) values ('Tabitha', 6, "F", "dark brown", "Nice", 1);
+insert into bears (name, age, gender, color, temperament, alive) values ('Sergeant Brown', 19, "M", "Green", "Slimy", 0);
+insert into bears (name, age, gender, color, temperament, alive) values ('Melissa', 13, "F", "dark brown", "goofy", 1);
+insert into bears (name, age, gender, color, temperament, alive) values ('Grinch', 2, "M", "Black", "Grinchy", 1);
+insert into bears (name, age, gender, color, temperament, alive) values ('Wendy', 6, "F", "Blue", "naive", 1);
+insert into bears (name, age, gender, color, temperament, alive) values (null, 20, "M", "black", "aggressive", 0);
