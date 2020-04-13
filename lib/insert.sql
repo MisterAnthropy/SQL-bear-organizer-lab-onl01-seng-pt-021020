60b52cc63905937388dab9d2dc216c90bb6ef575
Tyler Croley <tjcroley@outlook.com>
2020-04-13 22:16:36 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,6 +3,6 @@ insert into bears (name, age, gender, color, temperament, alive) values ('Rowdy'
 insert into bears (name, age, gender, color, temperament, alive) values ('Tabitha', 6, "F", "dark brown", "Nice", 1);
 insert into bears (name, age, gender, color, temperament, alive) values ('Sergeant Brown', 19, "M", "Green", "Slimy", 0);
 insert into bears (name, age, gender, color, temperament, alive) values ('Melissa', 13, "F", "dark brown", "goofy", 1);
-insert into bears (name, age, gender, color, temperament, alive) values ('Grinch', 2, null, null, null, null);
+insert into bears (name, age, gender, color, temperament, alive) values ('Grinch', 2, "M", "Black", "Grinchy", 1);
 insert into bears (name, age, gender, color, temperament, alive) values ('Wendy', 7, null, null, null, null);
 insert into bears (name, age, gender, color, temperament, alive) values (null, 10, null, null, null, null);
